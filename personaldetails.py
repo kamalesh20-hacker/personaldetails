6f521d4dd33445ec4fef16a3e691cbdc69202133
kamalesh.B <82257435+kamalesh20-hacker@users.noreply.github.com>
2021-04-18 20:03:16 +0530

    Add files via upload

---
+++ personaldetails.py
@@ -0,0 +1,22 @@
+name=str(input("Enter the name:"))
+age=int(input("Enter the age:"))
+address=str(input("Enter the address:"))
+salary=int(input("Enter the salary:"))
+number=int(input("Enter the number:"))
+print("NAME:",name)
+print("AGE:",age)
+print("ADDRESS:",address)
+print("SALARY:",salary)
+print("NUMBER",number)
+
+#output:
+Enter the name:kamalesh.B
+Enter the age:20
+Enter the address:Bangalore, Karnataka, India
+Enter the salary:100000
+Enter the number:9876543210
+NAME: kamalesh.B
+AGE: 20
+ADDRESS: Bangalore, Karnataka, India
+SALARY: 100000
+NUMBER 9876543210
